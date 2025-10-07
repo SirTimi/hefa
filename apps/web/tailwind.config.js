1077eff0fee67d14938b105dfa370b85d527a081
SirTimi <tobatimilehin1@gmail.com>
2025-10-07 17:03:04 +0100

    revamp of frontend

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-    './src/lib/**/*.{ts,tsx}',
-  ],
+  content: [],
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [],
 }
